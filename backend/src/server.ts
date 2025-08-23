06c282d7ab6a6e25e18aa55ee2efc7cc90c01746
Gyanendra Singh <gyanendrabaghel633@gmail.com>
2025-08-23 21:32:52 +0530

    feat: setup backend (#1)

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript + Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
